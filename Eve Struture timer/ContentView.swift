c2302b9170cd0aa09bcb4b04b421f166f2edacc3
Pascale Beaulac <pascale.beaulac@argon18.com>
2025-03-07 23:06:30 -0500

    Initial Commit

---
+++ Eve Struture timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Eve Struture timer
+//
+//  Created by Pascale on 2025-03-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Eve Struture timer/Eve_Struture_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eve_Struture_timerApp.swift
+//  Eve Struture timer
+//
+//  Created by Pascale on 2025-03-07.
+//
+
+import SwiftUI
+
+@main
+struct Eve_Struture_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
